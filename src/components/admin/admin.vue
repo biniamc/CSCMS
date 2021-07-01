2ccc8681b2657c9e243c8c8ee09819282a79753a
biniamc <biniamcoding@gmail.com>
2021-07-01 14:31:45 +0300

    some chnge

--- src/components/admin/admin.vue
+++ src/components/admin/admin.vue
@@ -54,6 +54,39 @@
           </div>
         </nav>
       <div class="mx-4"  style="margin-top: 5rem;">
+        <div class="vertical-center" style="margin-top: 1rem;">
+            <div class="inner-block">
+                <form>
+                    <h3>Sign Up</h3>
+
+                    <div class="form-group">
+                        <label>Full Name</label>
+                        <input type="text" class="form-control form-control-lg"/>
+                    </div>
+                    <div class="form-group">
+                        <label>BP Number</label>
+                        <input type="text" class="form-control form-control-lg" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Email address</label>
+                        <input type="email" class="form-control form-control-lg" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control form-control-lg" />
+                    </div>
+
+                    <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+
+                    <p class="forgot-password text-right">
+                        already register
+                        <router-link :to="{name: 'login'}">sign in?</router-link>
+                    </p>
+                </form>
+            </div>
+        </div>
 
       </div>
 
